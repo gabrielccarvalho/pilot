553486aa05cbd8b1058caa4e250ea1979ef3d6d3
Eduardo Pereira <eduardo.soares@pagar.me>
2020-03-13 07:21:29 -0300

    empty-state: add plataform documentation settings

---
+++ packages/pilot/src/containers/EmptyState/AccessDocs/documentationsSettings.js
@@ -0,0 +1,50 @@
+export default {
+  bizCommerce: {
+    link: 'pages.empty_state.access_docs_plataform.plataforms.biz_commerce.link',
+    plataformLabel: 'pages.empty_state.access_docs_plataform.plataforms.biz_commerce.label',
+  },
+  core: {
+    link: 'pages.empty_state.access_docs_plataform.plataforms.core.link',
+    plataformLabel: 'pages.empty_state.access_docs_plataform.plataforms.core.label',
+  },
+  ezCommerce: {
+    link: 'pages.empty_state.access_docs_plataform.plataforms.ez_commerce.link',
+    plataformLabel: 'pages.empty_state.access_docs_plataform.plataforms.ez_commerce.label',
+  },
+  iset: {
+    link: 'pages.empty_state.access_docs_plataform.plataforms.iset.link',
+    plataformLabel: 'pages.empty_state.access_docs_plataform.plataforms.iset.label',
+  },
+  lojaIntegrada: {
+    link: 'pages.empty_state.access_docs_plataform.plataforms.loja_integrada.link',
+    plataformLabel: 'pages.empty_state.access_docs_plataform.plataforms.loja_integrada.label',
+  },
+  magento: {
+    link: 'pages.empty_state.access_docs_plataform.plataforms.magento.link',
+    plataformLabel: 'pages.empty_state.access_docs_plataform.plataforms.magento.label',
+  },
+  openCart: {
+    link: 'pages.empty_state.access_docs_plataform.plataforms.open_cart.link',
+    plataformLabel: 'pages.empty_state.access_docs_plataform.plataforms.open_cart.label',
+  },
+  prestaShop: {
+    link: 'pages.empty_state.access_docs_plataform.plataforms.presta_shop.link',
+    plataformLabel: 'pages.empty_state.access_docs_plataform.plataforms.presta_shop.label',
+  },
+  signativa: {
+    link: 'pages.empty_state.access_docs_plataform.plataforms.signativa.link',
+    plataformLabel: 'pages.empty_state.access_docs_plataform.plataforms.signativa.label',
+  },
+  vtex: {
+    link: 'pages.empty_state.access_docs_plataform.plataforms.vtex.link',
+    plataformLabel: 'pages.empty_state.access_docs_plataform.plataforms.vtex.label',
+  },
+  woocommerce: {
+    link: 'pages.empty_state.access_docs_plataform.plataforms.woocommerce.link',
+    plataformLabel: 'pages.empty_state.access_docs_plataform.plataforms.woocommerce.label',
+  },
+  xtech: {
+    link: 'pages.empty_state.access_docs_plataform.plataforms.xtech.link',
+    plataformLabel: 'pages.empty_state.access_docs_plataform.plataforms.xtech.label',
+  },
+}
